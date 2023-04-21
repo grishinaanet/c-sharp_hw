cdf62c69a27cd85c97793deba8baacf65e687cfc
Anna <aniaanet@yandex.ru>
2023-04-22 02:52:20 +0300

    создание

--- homework_3/task_1/Program.cs
+++ homework_3/task_1/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// проверка
+﻿bool Palindrom(string arg)
+{
+    for (int i = 0; i < arg.Length / 2; i++)
+
+        if (arg[i] != arg[arg.Length - i - 1])
+            return false;
+    return true;
+}
+
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+string str = number.ToString();
+
+if(Palindrom(str) == true)
+{
+    Console.WriteLine("Палиндром");
+}
+else
+{
+    Console.WriteLine("не Палиндром");
+};
