d11ca29dc3843713c60c2099143f5ae7000af5f3
Anna <aniaanet@yandex.ru>
2023-04-30 13:38:52 +0300

    дз готово

---
+++ homework_6/task_1/Program.cs
@@ -0,0 +1,45 @@
+﻿void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void FillArrayKeyboard(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} число ");
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int numbersPositiv(int[] num)
+{   
+    int count = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        if (num[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+
+void Task1()
+{
+    Console.Write($"Сколько вы хотите ввести чисел: ");
+    int numbersSize = Convert.ToInt32(Console.ReadLine());
+    int size = numbersSize;
+    int[] numbers = new int[size];
+    FillArrayKeyboard(numbers);
+    Console.WriteLine("Массив");
+    PrintArray(numbers);
+    Console.WriteLine($"Положительных чисел = {numbersPositiv(numbers)}");
+}
+Task1();
---
+++ homework_6/task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿void intersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    Console.WriteLine("x = " + x);
+    double y_f = k1 * x + b1;
+    Console.WriteLine("y1 = " + y_f);
+    double y_s = k2 * x + b2;
+    Console.WriteLine("y2 = " + y_s);
+    Console.WriteLine($"({y_f},{y_s})");
+}
+
+double b1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+intersectionPoint(b1, k1, b2, k2);
